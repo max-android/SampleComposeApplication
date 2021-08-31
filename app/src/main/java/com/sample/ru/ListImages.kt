ad9a53509dd0e759be31413d87fbe583053d90ed
MaxAndroid <mobiledeveloperxxxl@gmail.com>
2021-08-31 15:54:07 +0300

    добавил описание данных

---
+++ app/src/main/java/com/sample/ru/ListImages.kt
@@ -0,0 +1,33 @@
+package com.sample.ru
+
+//лента еды
+const val butter = "https://foodish-api.herokuapp.com/images/butter-chicken/butter-chicken8.jpg"
+const val dessert = "https://foodish-api.herokuapp.com/images/dessert/dessert16.jpg"
+const val butter2 = "https://foodish-api.herokuapp.com/images/butter-chicken/butter-chicken3.jpg"
+const val dosa = "https://foodish-api.herokuapp.com/images/dosa/dosa20.jpg"
+const val idly = "https://foodish-api.herokuapp.com/images/idly/idly6.jpg"
+const val pasta = "https://foodish-api.herokuapp.com/images/pasta/pasta16.jpg"
+const val pizza = "https://foodish-api.herokuapp.com/images/pizza/pizza91.jpg"
+const val rice = "https://foodish-api.herokuapp.com/images/rice/rice5.jpg"
+const val samosa = "https://foodish-api.herokuapp.com/images/samosa/samosa11.jpg"
+
+
+
+
+//таб 1
+
+//горизонтальный список - новости
+//https://api.spaceflightnewsapi.net/v3/ARTICLES
+
+//горизонтальный слайдер мемов
+//https://api.doge-meme.lol/v1/memes/
+
+
+
+//портал для таба 2
+//https://picsum.photos/
+//галерея картинок
+//https://picsum.photos/v2/list
+
+//детали картинки
+//https://picsum.photos/id/0/info
