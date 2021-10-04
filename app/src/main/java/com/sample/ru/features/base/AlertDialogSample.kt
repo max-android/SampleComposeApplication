530dda665d88b2618221871a1db96288564f3230
MaxAndroid <mobiledeveloperxxxl@gmail.com>
2021-10-04 15:25:58 +0300

    добавлен Alert dialog

--- app/src/main/java/com/sample/ru/features/base/AlertDialogSample.kt
+++ app/src/main/java/com/sample/ru/features/base/AlertDialogSample.kt
@@ -1,12 +1,16 @@
 package com.sample.ru.features.base
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+import com.sample.ru.ui.theme.Shapes
 
 @Composable
 fun AlertDialogSample() {
@@ -55,4 +59,60 @@ fun AlertDialogSample() {
                 )
             }
         }
+}
+
+@Composable
+fun AppAlertDialog(
+    confirmButtonText : String,
+    dismissButtonText : String,
+    message : String,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+) {
+    val show = remember { mutableStateOf(true) }
+    if (show.value) {
+        AlertDialog(
+            modifier = Modifier.graphicsLayer {
+                shape = RoundedCornerShape(24.dp)
+                clip = true
+            },
+            onDismissRequest = {},
+            title = {
+                Text("title", color = MaterialTheme.colors.onPrimary)
+            },
+            shape = Shapes.large,
+            text = {
+                Text(text = message, color = MaterialTheme.colors.onPrimary)
+            },
+            backgroundColor = MaterialTheme.colors.primary,
+            properties = DialogProperties(
+                dismissOnBackPress = false,
+                dismissOnClickOutside = false
+            ),
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirm.invoke()
+                        show.value = false
+                    }) {
+                    Text(
+                        text = confirmButtonText,
+                        color = MaterialTheme.colors.secondary
+                    )
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        onDismiss.invoke()
+                        show.value = false
+                    }) {
+                    Text(
+                        text = dismissButtonText,
+                        color = MaterialTheme.colors.secondary
+                    )
+                }
+            }
+        )
+    }
 }
